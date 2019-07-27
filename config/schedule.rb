115eef68871c295299101cc993ae66b0a15c5983
Serge Vinogradoff <zoopyserg@gmail.com>
2019-07-27 22:17:20 +0300

    [CAP-32][#167553389] Applying good practices

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,17 +3,17 @@
 set :job_template, nil
 
 every :weekday, at: '5:00pm' do
-  command "caperoma report daily"
+  command 'caperoma report daily'
 end
 
 every :wednesday, at: '5:05pm' do
-  command "caperoma report three_day"
+  command 'caperoma report three_day'
 end
 
 every :friday, at: '5:05pm' do
-  command "caperoma report three_day"
+  command 'caperoma report three_day'
 end
 
 every :friday, at: '5:10pm' do
-  command "caperoma report weekly"
+  command 'caperoma report weekly'
 end
